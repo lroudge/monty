a83a5332c4fab5d764082a0f38a3fd91238461c6
lroudge <lauradrc@hotmail.fr>
2019-05-15 20:15:37 +0000

    correction of task 2

--- monty.h
+++ monty.h
@@ -79,7 +79,7 @@ extern data_t data;
 #define MALLOC_FAIL "Error: malloc failed\n"
 #define PUSH_FAIL "L%u: usage: push integer\n"
 #define PINT_FAIL "L%u: can't pint, stack empty\n"
-#define POP_FAIL "L%u: can't pop empty stack\n"
+#define POP_FAIL "L%u: can't pop an empty stack\n"
 #define SWAP_FAIL "L%u: can't swap, stack too short\n"
 #define ADD_FAIL "L%u: can't add, stack too short\n"
 
